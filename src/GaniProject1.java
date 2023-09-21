9700bb0521bab3d0bd1a2a91ae6f72e11227f11b
BT_1n3_22 <sample@bths.edu>
2023-09-21 08:51:32 -0400

    Snapshot 3

--- src/GaniProject1.java
+++ src/GaniProject1.java
@@ -6,11 +6,20 @@ public class GaniProject1 {
         System.out.print("How many people are in your group: ");
         int party = scan.nextInt();
         System.out.print("What's the tip percentage? (0-100): ");
-        int tip = scan.nextInt();
+        int percent = scan.nextInt();
         double cost = 0;
+        double bill = 0;
         while (cost!=-1) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             cost = scan.nextDouble();
+            bill += cost;
         }
+        double tip = bill * percent;
+        System.out.println("Total bill before tip: $" + bill);
+        System.out.println("Total percentage: $" + percent + "%");
+        System.out.println("Total tip: $" + (tip));
+        System.out.println("Total bill with tip: $" + (tip + bill));
+        System.out.println("Tip per person: $" + (tip / party));
+        System.out.println("Total cost per person: $" + ((bill + tip) / party));
     }
 }
